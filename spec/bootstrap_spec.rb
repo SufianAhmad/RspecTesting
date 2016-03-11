9d551b48e4194471c47ca8bed4fbe49406b3a73c
SufianAhmad <Sufian.Ahmad1@gmail.com>
2016-03-11 18:01:59 +0500

    basic tests

--- spec/bootstrap_spec.rb
+++ spec/bootstrap_spec.rb
@@ -20,4 +20,27 @@ describe Bootstrap do
     regular_expression = /^\w+@\w+\.com$/
     expect(email).to match regular_expression       
   end
+
+  it "assertion on truthiness" do
+    bool = true
+    falsy = false
+    expect(bool && falsy).to be true && false
+  end
+
+  it "asserts on predicates" do
+    class A
+      def good?
+        true
+      end
+    end
+    expect(A.new).to be_good
+  end
+
+  it "asserts on collections" do
+    list = [ :one, :two, :three, 4 ]
+
+    expect(list). to include 4
+    expect(list). to start_with :one
+    expect(list). to end_with 4
+  end
 end
